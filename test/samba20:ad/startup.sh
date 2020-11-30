d9d2017f0c4afa1ea0afcc67a0925d484f0c5cf0
isard <isard@f32-isard.edt.org>
2020-11-30 20:53:43 +0100

    samba ad

--- test/samba20:ad/startup.sh
+++ test/samba20:ad/startup.sh
@@ -34,23 +34,53 @@ echo -e "Samba03\nSamba03\n" | smbpasswd -a samba03
 
 # Configurar el client ldap
 #cp /opt/docker/ldap.conf /etc/openldap/ldap.conf
+cp /var/lib/samba/private/tls/ca.pem /etc/openldap/certs/.
 echo "BASE    dc=edt,dc=org" >> /etc/openldap/ldap.conf
 echo "URI     ldap://localhost" >> /etc/openldap/ldap.conf
 echo "TLS_CACERT /var/lib/samba/private/tls/ca.pem" >> /etc/openldap/ldap.conf
-#cp /var/lib/samba/private/tls/ca.pem /etc/openldap/certs/.
+echo "TLS_REQCERT allow" >> /etc/openldap/ldap.conf
 
 # Test kerberos
+echo "Test Kerberos"
 kinit
 klist
 kdestroy
 
 # Test DNS
+echo "Test DNS"
 host -t SRV _ldap._tcp.edt.org
 host -t SRV _kerberos._udp.edt.or
 host -t A edt.org
 host -t ns edt.org
 
 # Test samba
+echo "Test Samba"
 smbclient -L localhost -N
 smbclient //localhost/netlogon -UAdministrator -c 'ls'
+
+# Test LDAP
+echo "Test LDAP"
+ldapsearch -x  -LLL  -Z -D 'cn=Administrator,cn=Users,dc=edt,dc=org' -w Passw0rd -b 'CN=Users,dc=edt,dc=org' dn
+
+# Test winbind
+echo "test winbind"
+wbinfo -u
+wbinfo -g
+getent passwd administrator
+getent group EDT\\domain\ users
+wbinfo --pam-logon EDT\\administrator
+
+# ADUC
+# https://wiki.samba.org/index.php/Maintaining_Unix_Attributes_in_AD_using_ADUC
+#  Curses ADUC --> https://appimage.github.io/admin-tools/
+#  Appimage --> https://download.opensuse.org/repositories/home:/dmulder:/YaST:/AppImage/AppImage/admin-tools-latest-x86_64.AppImage.mirrorlist
+wget https://download.opensuse.org/repositories/home:/dmulder:/YaST:/AppImage/AppImage/admin-tools-latest-x86_64.AppImage
+mv admin-tools-latest-x86_64.AppImage admin-tools
+chmod +x admin-tools
+./admin-tools
+
+## RSAT
+# Administrar el AD des de windows remotament
+# https://wiki.samba.org/index.php/Installing_RSAT
+
 
